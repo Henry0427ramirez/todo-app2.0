4598ba56da8d31c6fc61e94a1ad26352bd12b4e3
Henry Ramirez <h.ramirez0427@gmail.com>
2015-04-21 09:05:17 -0700

    commit 1
    starting the todo project again

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Henry's 2.0 Todo List</title>
+<!--connecting to your files -->
+<script rel="stylesheet" type="text/css" href="css/main.css"></script>
+</head>
+<body>
+	<div class="wrap">
+		  
+	</div>
+ 
+</body>
+</html>
