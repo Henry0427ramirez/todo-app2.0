36fea34576b7941a74f34fe785e34f19562a0fdf
Henry Ramirez <h.ramirez0427@gmail.com>
2015-04-22 09:00:05 -0700

    commit 5
    finished adding the code from 6

--- includes/connect.php
+++ includes/connect.php
@@ -2,7 +2,7 @@
 //were connectint to localhost first to get to our root then our database that is todo2.0
 $mysqli = new mysqli('localhost', 'root', 'root', 'todo2.0');
 if ($mysqli->connect_error) {
-	die('connect error('. $mysqli->connect_errno . ')' .$mysqli->connect_error);
+	die('Connect Error('. $mysqli->connect_errno . ')' .$mysqli->connect_error);
 }
 else{
 //everything runs
--- index.php
+++ index.php
@@ -7,8 +7,16 @@
 </head>
 <body>
 	<div class="wrap">
-		  
+		  <div class="task-list">
+		  	<ul>
+		  	<?php 
+		  		require("includes/connect.php");
+		  	?>
+		  	</ul>
+		  </div>
+		  <form class="add-new-task" autocomplete="off">
+		  	<input type="text" name="new-task" placeholder="Add new Item..."/>
+		  </form>
 	</div>
- 
 </body>
 </html>
