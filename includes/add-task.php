694503ab7f995f2bf1f560d7211cbe03c88dbd10
Henry Ramirez <h.ramirez0427@gmail.com>
2015-04-23 08:36:35 -0700

    commit 11
    addded the add task

--- includes/add-task.php
+++ includes/add-task.php
@@ -3,7 +3,7 @@ $task = strip_tags($_POST["task"]);
 $date = date("Y-m-d");
 $time = date("H:i:s");
 include("connect.php");
-$mysqli = new mysqli("localhost", "root", "root", "todo");
+$mysqli = new mysqli("localhost", "root", "root", "todo2.0");
 $mysqli ->query("INSERT INTO tasks VALUES ('','$task', '$date', '$time')");
 $query = "SELECT * FROM tasks WHERE task='$task' and date='$date' and time='$time'";
 if($result = $mysqli->query($query)){
